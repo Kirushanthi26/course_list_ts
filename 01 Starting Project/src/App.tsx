937a4919169a625ee19086b5aacff9568e02cb35
kirushanthi26 <kirushanthikiru26@gmail.com>
2023-12-15 14:58:54 +0530

    useState and list

--- 01 Starting Project/src/App.tsx
+++ 01 Starting Project/src/App.tsx
@@ -1,16 +1,43 @@
 import CourseGoal from "./components/CourseGoal.tsx";
-import goalsImg from "./assets/goals.jpg"
+import goalsImg from "./assets/goals.jpg";
 import Header from "./components/Header.tsx";
+import { useState } from "react";
+
+interface CourseGoals {
+  title: string;
+  description: string;
+  id: number;
+}
 
 export default function App() {
+  const [goals, setGoals] = useState<CourseGoals[]>([]);
+
+  const addGoalHandler = () => {
+    setGoals((prevState) => {
+      const newCourseGoal: CourseGoals = {
+        id: Math.random(),
+        title: "learn React js",
+        description: "learn React js with ts, as soon as possible",
+      };
+      return [...prevState, newCourseGoal];
+    });
+  };
+
   return (
     <main>
-      <Header image={{src:goalsImg, alt:"header goal img"}}>
+      <Header image={{ src: goalsImg, alt: "header goal img" }}>
         <h1>Your Course Goals</h1>
       </Header>
-      <CourseGoal title="learn React and TS">
-        <p>learn basic typescript based react project</p>
-      </CourseGoal>
+      <button onClick={addGoalHandler}>Add Goal</button>
+      <ul>
+        {goals.map((goal) => (
+          <li key={goal.id}>
+            <CourseGoal title={goal.title}>
+              <p>{goal.description}</p>
+            </CourseGoal>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
